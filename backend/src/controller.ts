93b6e3b0d735983694e96430228274942fe023c8
Andrew Roberts <andrew@azroberts.com>
2024-11-30 12:28:17 -0500

    Cleaning up API

--- backend/src/controller.ts
+++ backend/src/controller.ts
@@ -1,408 +1,57 @@
 import express from "express";
 import { database } from "./firebase";
-import {
-  queryCompanyData,
-  queryIndices,
-  queryTickers,
-  TickerResult,
-} from "./polygon";
+import { queryCompanyData } from "./polygon";
 
-class MyObject {
-  constructor(public msg: string, public value: number = 42) {}
-}
 
+// HELPER FUNCTIONS -- DO NOT EXPORT THESE
+
+// shorthand type aliases for express functions
 type Req = express.Request;
 type Res = express.Response;
 
-export class newController {
-  public async getTicker(req: Req, res: Res): Promise<void> {
-    const { ticker, start, end } = req.params;
-    
-    const stockData = await queryCompanyData({
-      ticker,
-      from: start,
-      to: end
-    });
+// Used by any express endpoints that need user authorization
+const getAuthentication = async () => {
 
-    res.send(stockData);
-  }
 }
 
-export class Controller {
-  //returns something.
-  public getHello(req: express.Request, res: express.Response): void {
-    res.send(new MyObject("hello world"));
-  }
-
-  //returns whatever you post to it.  You can use the contents of req.body to extract information being sent to the server
-  public postHello(req: express.Request, res: express.Response): void {
-    res.send({ body: req.body });
-  }
-
-  public async firebaseTest(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    const ref = database.ref("/test-key");
-    const value = (await ref.get()).val();
-    res.send({ value });
-  }
-
-  public async postFirebase(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const ref = database.ref("/Apple");
-      const message = req.body.message;
-      await ref.set({
-        message: message,
-        timestamp: new Date().toISOString(),
-      });
-      res.send({ success: true, message: "Posted to Firebase!" });
-    } catch (error) {
-      const err = error as Error;
-      console.log("Error posting to Firebase: ", err);
-      res.status(500).send({ success: false, error: err.message });
-    }
-  }
-
-  // public async stockTest(
-  //   req: express.Request,
-  //   res: express.Response
-  // ): Promise<void> {
-  //   try {
-  //     // Extract parameters from the request body, with defaults
-  //     const ticker = req.body.ticker || "AAPL"; // Default to "AAPL"
-  //     const from = req.body.from || "2024-11-11"; // Default to "2024-11-10"
-  //     const to = req.body.to || "2024-11-15"; // Default to "2024-11-15"
-  //     const interval = req.body.interval || "day"; // Default to "day"
-
-  //     // Fetch stock data
-  //     const result = await queryCompanyDataNoCache({
-  //       ticker,
-  //       from,
-  //       to,
-  //       interval,
-  //     });
-
-  //     // Write data to Firebase
-  //     const ref = database.ref(`/stocks/${ticker}`);
-  //     await ref.set({ data: result });
-
-  //     // Send success response
-  //     res.send({
-  //       success: true,
-  //       message: `Posted ${ticker} stock data to Firebase!`,
-  //     });
-  //   } catch (error) {
-  //     const err = error as Error;
-  //     console.error("Error posting stocks to Firebase: ", err);
-  //     res.status(500).send({ success: false, error: err.message });
-  //   }
-  // }
-
-  public async postAllStocks(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const date = req.body.date || "2024-11-15";
-      const result = await queryTickers(date);
-      const sanitizedResults = result.results.map((ticker) => {
-        const sanitizedTicker: Record<string, any> = { ...ticker };
-        Object.keys(sanitizedTicker).forEach((key) => {
-          if (sanitizedTicker[key] === undefined) {
-            delete sanitizedTicker[key];
-          }
-        });
-        return sanitizedTicker as TickerResult;
-      });
-
-      const sanitizedResult = { ...result, results: sanitizedResults };
-
-      const ref = database.ref(`/all`);
-      await ref.set({ stocks: sanitizedResult });
-      res.send({ success: true, message: `Posted all stocks to the Firebase` });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error posting all stocks to Firebase: ", err);
-      res.status(500).send({ success: false, error: err.message });
-    }
-  }
-
-  public async getAllStocks(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const ref = database.ref(`/all/stocks/results`);
-      const snapshot = await ref.once("value");
-      if (snapshot.exists()) {
-        const stockData = snapshot.val();
-        res.send({ success: true, data: stockData });
-      } else {
-        res
-          .status(404)
-          .send({ success: false, message: "No stock data found." });
-      }
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error retrieving stock data:", err);
-      res.status(500).send({ success: false, error: err.message });
-    }
-  }
-
-  public async postUserStock(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const { userId, stockSymbol, shares, price } = req.body;
-
-      if (!userId || !stockSymbol || !shares || !price) {
-        res.status(400).send({
-          success: false,
-          message:
-            "Missing required fields: userId, stockSymbol, shares, or price",
-        });
-      }
-
-      const ref = database.ref(`/users/${userId}/stocks`);
-
-      const stockPurchase = {
-        stockSymbol,
-        shares,
-        price,
-        timestamp: new Date().toISOString(),
-      };
-
-      await ref.push(stockPurchase);
-
-      res.send({
-        success: true,
-        message: "Stock purchase recorded successfully!",
-        stockPurchase,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error posting stock to Firebase: ", err);
-      res.status(500).send({ success: false, error: err.message });
-    }
-  }
-
-  public async getUserStock(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const { userId } = req.query;
-
-      if (!userId) {
-        res.status(400).send({
-          success: false,
-          message: "Missing required field: userId",
-        });
-        return;
-      }
-
-      const ref = database.ref(`/users/${userId}/stocks`);
-      const stocksSnapshot = await ref.get();
-      const stocks = stocksSnapshot.val();
-
-      if (!stocks) {
-        res.status(404).send({
-          success: false,
-          message: "No stocks found for the given userId",
-        });
-        return;
-      }
-
-      res.send({
-        success: true,
-        message: "Stocks fetched successfully!",
-        stocks,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error fetching user stocks: ", err);
-      res.status(500).send({ success: false, error: err.message });
-    }
-  }
-
-  public async updateUserStock(
-    req: express.Request,
-    res: express.Response
-  ): Promise<void> {
-    try {
-      const { userId, stockSymbol, shares, price, stockId } = req.body;
-
-      if (!userId || !stockId || !stockSymbol || !price) {
-        res.status(400).send({
-          success: false,
-          message:
-            "Missing required fields: userId, stockId, stockSymbol, shares, or price",
-        });
-        return;
-      }
-
-      const ref = database.ref(`/users/${userId}/stocks/${stockId}`);
-
-      if (shares <= 0) {
-        await ref.remove();
-        res.status(200).send({
-          success: true,
-          message: "All shares sold, removing entry",
-        });
-        return;
-      }
-
-      const stockUpdate = {
-        stockSymbol,
-        shares,
-        price,
-        timestamp: new Date().toISOString(),
-      };
-
-      await ref.update(stockUpdate);
-
-      res.send({
-        success: true,
-        message: "Stock updated successfully!",
-        stockUpdate,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error updating stock in Firebase: ", err.message);
-
-      res.status(500).send({
-        success: false,
-        message: "Internal server error",
-        error: err.message,
-      });
-    }
-  }
-
-  public async queryIndicesExpress(
-    req: express.Request,
-    res: express.Response
-  ) {
-    try {
-      const { date } = req.query;
-
-      if (!date) {
-        res.status(400).send({
-          success: false,
-          message: "Missing required field: date",
-        });
-        return;
-      }
-
-      const response = await queryIndices(date.toString());
-      if (!response) {
-        res.status(400).send({
-          success: false,
-          message: "No response from given date.",
-        });
-      }
-      res.send({
-        success: true,
-        message: "Index fetched successfully!",
-        response,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error fetching index data: ", err.message);
-      res.status(500).send({
-        success: false,
-        message: "Internal server error",
-        error: err.message,
-      });
-    }
-  }
-
-  public async queryAllTickers(req: express.Request, res: express.Response) {
-    try {
-      const { date } = req.query;
-
-      if (!date) {
-        res.status(400).send({
-          success: false,
-          message: "Missing required field: date",
-        });
-        return;
-      }
-
-      const response = await queryTickers(date.toString());
-
-      if (!response) {
-        res.status(400).send({
-          success: false,
-          message: "No response from given date.",
-        });
-      }
-      res.send({
-        success: true,
-        message: "Stocks fetched successfully!",
-        response,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error fetching stock data: ", err.message);
-      res.status(500).send({
-        success: false,
-        message: "Internal server error",
-        error: err.message,
-      });
-    }
-  }
-
-  public async getUserCurrency(req: express.Request, res: express.Response) {
-    try {
-      const { userId } = req.query;
-      const ref = database.ref(`/users/${userId}`);
-
-      const snapshot = await ref.get();
-      const currency = snapshot.val();
-
-      console.log("snapshot: ", snapshot);
-      console.log("currency: ", currency);
-
-      res.send({
-        success: true,
-        message: "Currency fetched successfully!",
-        currency,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error fetching stock data: ", err.message);
-      res.status(500).send({
-        success: false,
-        message: "Internal server error",
-        error: err.message,
-      });
-    }
-  }
-
-  public async updateCurrency(req: express.Request, res: express.Response) {
-    try {
-      const { userId, currency } = req.body;
-      const ref = database.ref(`/users/${userId}/`);
-      await ref.update({ currency: currency });
-
-      res.send({
-        success: true,
-        message: "Currency updated successfully!",
-        currency,
-      });
-    } catch (error) {
-      const err = error as Error;
-      console.error("Error updating currency: ", err.message);
-      res.status(500).send({
-        success: false,
-        message: "Internal server error",
-        error: err.message,
-      });
-    }
-  }
+// Used by buyStock & sellStock to execute trades
+const exchangeStock = async () => {
+  
 }
+
+
+
+// EXPRESS ROUTES -- EXPORT THESE
+
+// Express route for retrieving price information about a stock
+const getStock = async (req: Req, res: Res): Promise<void> => {
+  const { ticker, start, end } = req.params;
+  const stockData = await queryCompanyData({
+    ticker,
+    from: start,
+    to: end
+  });
+  res.send(stockData);
+}
+
+// Express route for getting all information about a user's portfolio
+const getPortfolio = async (req: Req, res: Res): Promise<void> => {
+
+}
+
+// Express route for purchasing a stock
+const buyStock = async (req: Req, res: Res): Promise<void> => {
+
+}
+
+// Express route for selling a stock
+const sellStock = async (req: Req, res: Res): Promise<void> => {
+
+}
+
+// Express route to sign-in a user
+const login = async (req: Req, res: Res): Promise<void> => {
+
+}
+
+export { getStock, getPortfolio, buyStock, sellStock, login }
--- backend/src/polygon.ts
+++ backend/src/polygon.ts
@@ -20,6 +20,7 @@ interface Price {
 }
 
 interface CompanyResponse {
+  successful: boolean;
   ticker: string;
   count: number;
   start: Date;
@@ -27,44 +28,9 @@ interface CompanyResponse {
   prices: Price[];
 }
 
-interface PortfolioResponse {}
-
-interface TickerResult {
-  T: string; // Ticker symbol
-  c: number; // Close price
-  h: number; // High price
-  l: number; // Low price
-  n: number; // Number of trades
-  o: number; // Open price
-  t: number; // Timestamp
-  v: number; // Volume
-  vw: number; // Volume weighted average price
-}
-
-interface TickerResponse {
-  adjusted: boolean;
-  queryCount: number;
-  results: TickerResult[];
-  resultsCount: number;
-  status: string;
-}
-
-interface IndexData {
-  symbol: string;
-  date: string;
-  open: number;
-  close: number;
-  high: number;
-  low: number;
-  volume: number;
-}
-
-interface IndicesResponse {
-  nasdaq: IndexData;
-}
-
 const POLYGON_API_KEY = environment.POLYGON_API_KEY || "";
 
+// Directly queries Polygon API for price information about a company/stock
 const queryCompanyDataNoCache = async ({
   ticker,
   from = new Date().toISOString().substring(0, 10),
@@ -73,7 +39,7 @@ const queryCompanyDataNoCache = async ({
 }: CompanyQueryParams): Promise<CompanyResponse> => {
   const timespan = interval === "half-hour" ? "minute" : interval;
   const multiplier = interval === "half-hour" ? 30 : 1;
-  console.log(POLYGON_API_KEY);
+
   const data = await fetch(
     `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/${multiplier}/${timespan}/${from}/${to}`,
     {
@@ -116,6 +82,7 @@ const queryCompanyDataNoCache = async ({
   return data;
 };
 
+// Retrieves price information about a company/stock (checks firebase as cache)
 const queryCompanyData = async ({
   ticker,
   from = new Date().toISOString().substring(0, 10),
@@ -126,64 +93,6 @@ const queryCompanyData = async ({
   return queryCompanyDataNoCache({ ticker, from, to, interval });
 };
 
-const queryPortfolio = async ({}: QueryParams) => {};
-
-// TODO: Figure out a way to list stocks for the current day. When the current day is input, this response is given:
-//{"status":"NOT_AUTHORIZED","request_id":"889c5f07cc40f464b68f8a87098b170e","message":"Attempted to request today's data before end of day. Please upgrade your plan at https://polygon.io/pricing"}
-const queryTickers = async (date: string): Promise<TickerResponse> => {
-  const response = await fetch(
-    `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${date}?adjusted=true&apiKey=${POLYGON_API_KEY}`
-  );
-
-  const res = await response.json();
-  return {
-    adjusted: res.adjusted,
-    queryCount: res.queryCount,
-    results: res.results.map((ticker: any) => ({
-      T: ticker.T,
-      c: ticker.c,
-      h: ticker.h,
-      l: ticker.l,
-      n: ticker.n,
-      o: ticker.o,
-      t: ticker.t,
-      v: ticker.v,
-      vw: ticker.vw,
-    })),
-    resultsCount: res.resultsCount,
-    status: res.status,
-  };
-};
-
-const queryIndices = async (date: string): Promise<IndicesResponse> => {
-  const nasdaq_response = await fetch(
-    `https://api.polygon.io/v1/open-close/I:COMP/${date}?apiKey=${POLYGON_API_KEY}`
-  );
-
-  const nasdaq_res = await nasdaq_response.json();
-
-  console.log(nasdaq_res);
-
-  const formatResponse = (data: any): IndexData => ({
-    symbol: data.symbol,
-    date: data.from,
-    open: data.open,
-    close: data.close,
-    high: data.high,
-    low: data.low,
-    volume: data.volume,
-  });
-
-  return {
-    nasdaq: formatResponse(nasdaq_res),
-  };
-};
-
 export {
   queryCompanyData,
-  queryCompanyDataNoCache,
-  queryPortfolio,
-  queryTickers,
-  queryIndices,
 };
-export type { TickerResult, TickerResponse };
